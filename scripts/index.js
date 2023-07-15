b1d85115df1749faf5e6379512dd8f30cc1e380f
IMLUOAI <imluoai@gmail.com>
2023-07-15 14:24:23 -0500

    update after stage 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,33 @@
 const initialCards = [
   {
-    title: "Lonely Earth",
+    name: "Lonely Earth",
     link: "https://unsplash.com/photos/full-moon-during-day-yOIT88xWkbg",
   },
 
   {
-    title: "Boys Bathing",
+    name: "Boys Bathing",
     link: "https://unsplash.com/photos/a-group-of-people-standing-next-to-each-other-SLtSqhLypuc",
   },
 
   {
-    title: "Cloudy Sunrise",
+    name: "Cloudy Sunrise",
     link: "https://unsplash.com/photos/9A5wyDXopWI",
   },
 
   {
-    title: "Teran",
+    name: "Teran",
     link: "https://unsplash.com/photos/tmD2T6L4cWA",
   },
 
   {
-    title: "Flowers",
+    name: "Flowers",
     link: "https://unsplash.com/photos/a-couple-of-flowers-that-are-in-a-vase-n7XxYtxp23M",
   },
 
   {
-    title: "No face to be shown",
+    name: "No face to be shown",
     link: "https://unsplash.com/photos/a-woman-sitting-on-a-couch-with-a-paper-bag-on-her-head-479rJrzXXQw",
   },
 ];
+
 console.log(initialCards);
