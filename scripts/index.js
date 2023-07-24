af6d78f34073fc506cccec630f37bb927b2dfb9a
IMLUOAI <imluoai@gmail.com>
2023-07-24 01:33:21 -0500

    update stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,34 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = document.querySelector(".modal__close-button");
+// const profileTitleInput = document.querySelector("#profile-title-input");
+// const profileDescriptionInput = document.querySelector(
+//   "#profile-description-input"
+// );
+// const profileModalForm = document.querySelector(".modal__form");
+// function closePopup() {
+//   profileEditModal.classList.remove("modal-opened");
+// }
+
+profileEditButton.addEventListener("click", () => {
+  // profileNameInput.value = profileName.textContent;
+  // profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
+
+// profileEditButton.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   profileTitle.textContent = profileTitleInput.value;
+//   profileDescription.textContent = profileDescriptionInput.value;
+//   closePopup();
+// });
