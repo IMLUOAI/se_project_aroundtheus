26fe71da07fcde61851084944a037d745ca3e60b
IMLUOAI <imluoai@gmail.com>
2023-07-30 00:12:36 -0500

    js complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,36 @@
 const initialCards = [
   {
     name: "Lonely Earth",
-    link: "https://unsplash.com/photos/full-moon-during-day-yOIT88xWkbg",
+    link: "https://images.unsplash.com/photo-1507499739999-097706ad8914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80",
   },
 
   {
     name: "Boys Bathing",
-    link: "https://unsplash.com/photos/a-group-of-people-standing-next-to-each-other-SLtSqhLypuc",
+    link: "https://images.unsplash.com/photo-1688649102473-099b9d16aa32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=761&q=80",
   },
 
   {
     name: "Cloudy Sunrise",
-    link: "https://unsplash.com/photos/9A5wyDXopWI",
+    link: "https://images.unsplash.com/photo-1680100612420-e57b14dd2c7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
   },
 
   {
     name: "Teran",
-    link: "https://unsplash.com/photos/tmD2T6L4cWA",
+    link: "https://images.unsplash.com/photo-1688634219076-aa0815bfa7b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=699&q=80",
   },
 
   {
     name: "Flowers",
-    link: "https://unsplash.com/photos/a-couple-of-flowers-that-are-in-a-vase-n7XxYtxp23M",
+    link: "https://images.unsplash.com/photo-1687431343219-2ea06211d6bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
   },
 
   {
     name: "No face to be shown",
-    link: "https://unsplash.com/photos/a-woman-sitting-on-a-couch-with-a-paper-bag-on-her-head-479rJrzXXQw",
+    link: "https://images.unsplash.com/photo-1689008365999-fa5224b16326?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=754&q=80",
   },
 ];
 
-// console.log(initialCards);
+console.log(initialCards);
 
 /* Elements */
 
@@ -73,22 +73,25 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardTitleElement = cardElement.querySelector(".card__title");
-  cardTitleElement.textContent = "cardData.name";
-  cardImageElement.alt = "cardData.name";
-  cardImageElement.src = "cardData.link";
+  cardTitleElement.textContent = cardData.name;
+  cardImageElement.alt = cardData.name;
+  cardImageElement.src = cardData.link;
   return cardElement;
 }
 
 /* Event Listeners */
 
-profileOpenButton.addEventListener("click", toggleForm);
-profileCloseButton.addEventListener("click", toggleForm);
-profileModalForm.addEventListener("submit", handleProfileModalFormSubmit);
+function addCardsToPage(initialCards) {
+  const galleryCardsElement = document.querySelector(".gallery__cards");
 
-// for (i = 0; i < initialCards.length; i++) {
-//   console.log(initialCards[i]);
-// }
-initialCards.forEach((cardData) => {
-  const cardElement = getCardElement(cardData);
-  galleryCardsElement.append(cardElement);
-});
+  profileOpenButton.addEventListener("click", toggleForm);
+  profileCloseButton.addEventListener("click", toggleForm);
+  profileModalForm.addEventListener("submit", handleProfileModalFormSubmit);
+
+  initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
+    galleryCardsElement.append(cardElement);
+  });
+}
+
+addCardsToPage(initialCards);
